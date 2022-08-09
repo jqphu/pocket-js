159b57ecc57b7f5e503d0fc57fe6bb2a2f9de233
Justin Phu <me@justinphu.dev>
2022-08-09 13:24:58 +0800

    feat: add asset changes type

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -1,2 +1,5 @@
 export * from './simulation';
 export * from './transaction';
+
+// Only export this type as it will conflict with types in Simulation.
+export { AssetChanges } from './asset_changes';
